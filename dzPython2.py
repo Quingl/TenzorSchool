35e8334dd96cd514a3e3906bdd0bd8bfe0f45b34
Quingl <dandov@inbox.ru>
2021-11-24 23:27:18 +0300

    Домашнее задание 2 Python (Дандов Данил)

---
+++ dzPython2.py
@@ -0,0 +1,25 @@
+#1
+print("Введите два числа(можно дробные): ")
+first_number, second_number = float(input()), float(input())
+print("Результат сложения чисел: ", first_number + second_number)
+print("Результат вычитания чисел: ", first_number - second_number)
+print("Результат умножения чисел: ", first_number * second_number)
+print("Результат деления чисел: ", first_number / second_number)
+print("Результат возведения в степень чисел: ", first_number ** second_number)
+print("Результат деления по модулю чисел: ", first_number % second_number)
+print("Результат целочисленного деления чисел: ", first_number // second_number)
+
+#2
+print("Введите ваше имя: ")
+name = input()
+print("Здравствуйте, %s, рады знакомству!!!1!111!1!!" %(name))
+
+#3
+print("Введите любую строку: ")
+print("Выводим последние два символа этой строки в обратном порядке:", input()[-1:-3:-1])
+
+#4
+import math
+print("Введите радиус круга: ")
+radius = float(input())
+print("Площадь круга = ", math.pi * radius**2)
